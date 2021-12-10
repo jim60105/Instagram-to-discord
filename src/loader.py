ce7bd617e58406a1e4a93eaa140c0e2c1225bddd
陳鈞 <jim60105@gmail.com>
2021-12-10 14:29:39 +0800

    Correct using Inheritance in python

--- src/loader.py
+++ src/loader.py
@@ -4,17 +4,18 @@ from instaloader import instaloader
 
 class Loader(instaloader.Instaloader):
     def __init__(self, username, password):
+        super().__init__()
         self.username = username
         self.password = password
         if username and password:
             self.should_login = self.login()
-    
+
     def login(self) -> bool:
         isLogin = False
         try:
             if self.username and self.password:
-                self.login(self.username, self.password)
-                isLogin = self.test_login() == self.username
+                super().login(self.username, self.password)
+                isLogin = super().test_login() == self.username
         except instaloader.BadResponseException:
             print(f'BadResponceException: This happens when your account is blocked by Instagram. Log in to the app to check what happened.')
         finally:
