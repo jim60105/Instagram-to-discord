df6c02eb2117d30c5f2dfc425c8015f7e92714ee
陳鈞 <jim60105@gmail.com>
2021-12-18 22:36:35 +0800

    Change std logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from typing import List
 
@@ -8,25 +9,26 @@ from src.loader import Loader
 
 
 if __name__ == "__main__":
+    print('== Instagram to Discord ==')
     config = Config()
 
     if len(config.users) == 0 or not config.webhook_url:
-        print('Please set the config file properly!')
+        print('Please set the config file properly!', file=sys.stderr)
         exit()
 
     DB.init_db()
 
     L = Loader(config.login_username, config.login_password)
 
-    print('Project start!')
-
     loops: List[Loop] = []
     for username in config.users:
         print(f'Add monitor: {username}')
         loops.append(Loop(config, username, L))
 
+    delay = config.delay or 300
+    print(f'Interval: {delay} seconds.')
     print(f'Complete initialization. Start monitoring...')
     while True:
         for loop in loops:
-            time.sleep(config.delay or 300)
+            time.sleep(delay)
             loop.run()
--- src/loader.py
+++ src/loader.py
@@ -1,4 +1,4 @@
-
+import sys
 from instaloader import instaloader
 
 
@@ -16,17 +16,17 @@ class Loader(instaloader.Instaloader):
                 super().login(self.username, self.password)
                 isLogin = True
         except instaloader.BadResponseException as e:
-            print(e)
-            print(f'BadResponceException: This happens when your account is blocked by Instagram. Login from your mobile phone app to check what happened.')
+            print(e, file=sys.stderr)
+            print(f'BadResponceException: This happens when your account is blocked by Instagram. Login from your mobile phone app to check what happened.', file=sys.stderr)
         except instaloader.ConnectionException as e:
-            print(e)
-            print(f'If you get 429, DO NOT login the account {self.username} from multiple instances at the same time. Neither from this program, nor from your mobile phone or PC.')
+            print(e, file=sys.stderr)
+            print(f'If you get 429, DO NOT login the account {self.username} from multiple instances at the same time. Neither from this program, nor from your mobile phone or PC.', file=sys.stderr)
         except instaloader.TwoFactorAuthRequiredException as e:
-            print(e)
-            print(f'This program does not support 2FA login. Please disable this feature on account {self.username}.')
+            print(e, file=sys.stderr)
+            print(f'This program does not support 2FA login. Please disable this feature on account {self.username}.', file=sys.stderr)
         finally:
             if isLogin:
                 print(f'Login as {self.username}')
             else:
-                print(f'Login failed: {self.username}')
+                print(f'Not login or login failed', file=sys.stderr)
             return isLogin
