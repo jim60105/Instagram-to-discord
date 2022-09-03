9be0c4c4bf77f27ae6746a50517bddadafc4ce40
陳鈞 <jim60105@gmail.com>
2022-09-04 04:00:38 +0800

    Add user-agent

--- src/loader.py
+++ src/loader.py
@@ -4,7 +4,7 @@ from instaloader import instaloader
 
 class Loader(instaloader.Instaloader):
     def __init__(self, username, password):
-        super().__init__()
+        super().__init__(user_agent='Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 Instagram 250.0.0.21.109 Android (29/10; 440dpi; 1080x2210; Xiaomi; Mi 9T Pro; raphael; qcom; zh_TW; 394071295)')
         self.username = username
         self.password = password
         self.should_login = self.login()
